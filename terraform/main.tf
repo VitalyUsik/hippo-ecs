a7d730a35467ffe5c4c4cc417c2107456c8edf5e
vitalyusik <vitalyusik1989@gmail.com>
2024-06-16 12:31:28 -0300

    Add execution role

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,6 +118,28 @@ resource "aws_lb_listener" "http_listener" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.environment}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${var.environment}-cluster"
 }
@@ -126,6 +148,7 @@ resource "aws_ecs_task_definition" "main" {
   family                   = "${var.environment}-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   cpu                      = "256"
   memory                   = "512"
 
