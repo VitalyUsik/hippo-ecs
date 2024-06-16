b339dcfd7941f63a694fe63430abc14f4a0443c0
vitalyusik <vitalyusik1989@gmail.com>
2024-06-16 13:34:18 -0300

    Try with public subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,12 +32,14 @@ resource "aws_subnet" "main_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = element(data.aws_availability_zones.available.names, 0)
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "main_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = element(data.aws_availability_zones.available.names, 1)
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "main" {
