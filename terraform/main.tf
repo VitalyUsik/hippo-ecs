c4517e1af1cf0cc21277c8098b4694c62e3f126d
vitalyusik <vitalyusik1989@gmail.com>
2024-06-16 12:14:21 -0300

    Expose port, switch to dynamic port mapping

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,7 +135,7 @@ resource "aws_ecs_task_definition" "main" {
     essential = true
     portMappings = [{
       containerPort = 80
-      hostPort      = 80
+      hostPort      = 0
       protocol      = "tcp"
     }]
   }])
